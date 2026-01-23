e1c5da8a2315257c178b2e42497f5317264c0d6e
Tamizhbharathi-rajkamal <59680210+Tamizhbharathi-rajkamal@users.noreply.github.com>
2026-01-23 21:18:27 +0530

    numberType

---
+++ numberType.js
@@ -0,0 +1,11 @@
+let num=0
+
+if(num>0){
+    console.log("It is a Positive Number")
+}else if(num == 0){
+    console.log("It is a Natural Number")
+}
+else{
+    console.log("It is a Negative Number")
+}
+
